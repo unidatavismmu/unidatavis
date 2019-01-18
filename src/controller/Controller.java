8568bcf48834d41cfd6c88877147a04412e7c764
comp.sci61 <comp.sci61@gmail.com>
2019-01-18 11:58:04 +0000

    Added java files

---
+++ src/controller/Controller.java
@@ -0,0 +1,28 @@
+package controller;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class Controller {
+	
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8005);
+		WebAppContext ctx = new WebAppContext();
+		ctx.setResourceBase("webapp");
+		ctx.setContextPath("/");
+		Configure(server);
+		ctx.addServlet("HomeServlet", "/");
+		server.setHandler(ctx);
+		server.start();
+		server.join();
+	}
+	
+	private static void Configure(Server server) {
+		org.eclipse.jetty.webapp.Configuration.ClassList classList = 
+				org.eclipse.jetty.webapp.Configuration.ClassList.setServerDefault(server);
+		
+		classList.addAfter("org.eclipse.jetty.webapp.FragmentConfiguration", "org.eclipse.jetty.plus.webapp.EnvConfiguration", "org.eclipse.jetty.plus.webapp.PlusConfiguration");
+		classList.addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration", "org.eclipse.jetty.annotations.AnnotationConfiguration");
+	}
+
+}
